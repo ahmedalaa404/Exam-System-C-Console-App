67c40f809ebf5e7dd4ff5ba019fd127202f57959
ahmed alaa yassin <93731317+ahmedalaa404@users.noreply.github.com>
2023-08-01 04:16:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exam_System_C__Console_App
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ QuestionsBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_System_C__Console_App
+{
+	public  class QuestionsBase
+	{
+	}
+}
